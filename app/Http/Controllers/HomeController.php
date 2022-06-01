25320443dd09b7d6858b49b305c4c853ea2eac96
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-06-01 14:27:43 +0600

    Display food menu From Database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,20 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Food;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view("home");
+        $data = Food::all();
+        return view("home",compact("data"));
     }
     public function redirect()
     {
+        $data = Food::all();
         $userType = Auth::user()->usertype;
         if($userType=='1'){
             return view('admin.adminHome');
         }else{
-            return view('home');
+            return view('home',compact("data"));
         }
     }
 }
